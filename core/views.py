6b9da90b2d5c5c947a64afe1b513756910590341
Brian Christensen <brianc@wi.rr.com>
2020-11-20 10:11:04 -0600

    rm core

--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# This view serves the React frontend app
-
-def index(request):
-    return render(request, 'build/index.html')
-
-def manifest(request):
-    return render(request, 'build/manifest.json')
